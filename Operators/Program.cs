ce0972dbdb038d07dcab68eeab1a2c8a93afaad1
PARTH SARTHI <parthsarthi2003.ps@gmail.com>
2021-05-12 22:02:31 +0530

    initial commit.

---
+++ Operators/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+namespace Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 27;
+            var b = 3;
+            var c = 17;
+            var d = 2;
+            Console.WriteLine(a+b/d-c);
+            Console.WriteLine(a > b); Console.WriteLine(a!=c);
+            Console.WriteLine((float)c/(float)d);
+            Console.WriteLine(a + b);
+            Console.WriteLine(a - b);
+            Console.WriteLine(a*b);
+            Console.WriteLine(a/b);
+
+        }
+    }
+}
