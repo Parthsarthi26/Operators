71ff93f334374ad3afa7f4c2d3d699be16d7c4c3
Abhishek Kumar <abhishekatgit@gmail.com>
2021-05-12 22:16:49 +0530

    first change.

--- Operators/Program.cs
+++ Operators/Program.cs
@@ -11,6 +11,8 @@ namespace Operators
             var b = 3;
             var c = 17;
             var d = 2;
+            
+            // added by abhishek.
             Console.WriteLine(a+b/d-c);
             Console.WriteLine(a > b); Console.WriteLine(a!=c);
             Console.WriteLine((float)c/(float)d);
