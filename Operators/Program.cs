0ebf5dda3b6c76a6b3416acd977fcb2096d522ae
PARTH SARTHI <parthsarthi2003.ps@gmail.com>
2021-05-12 22:29:31 +0530

    changed

--- Operators/Program.cs
+++ Operators/Program.cs
@@ -13,7 +13,7 @@ namespace Operators
             var d = 2;
             
             // added by abhishek.   
-            Console.WriteLine(a+b/d-c);
+            Console.WriteLine(a+c/d-b);
             Console.WriteLine(a > b); Console.WriteLine(a!=c);
             Console.WriteLine((float)c/(float)d);
             Console.WriteLine(a + b);
